3df59aaa245da5d9f98539dd9a80871235cfba77
Ryker Swensen <83606669+RykerSwensen@users.noreply.github.com>
2023-10-21 16:25:21 -0600

    messing with mongoose

--- db/user.ts
+++ db/user.ts
@@ -2,16 +2,22 @@ import mongoose, { Document, Schema } from 'mongoose';
 
 
 interface IUser extends Document {
-  username: string;
-  password: string;
+  username: { 
+    type: string,
+    required: [true, 'Username is required.']},
+  password: {
+    type: string,
+    required: [true, 'Password is required.']};
   displayName: string;
   email: string;
   joinDate: string;
 }
 
 const UserSchema: Schema = new Schema({
-  username: { type: String },
-  password: { type: String },
+  username: { type: String,
+    required: [true, 'Username is required.'] },
+  password: { type: String,
+    required: [true, 'Password is required.']},
   displayName: { type: String },
   email: { type: String },
   joinDate: { type: String },
