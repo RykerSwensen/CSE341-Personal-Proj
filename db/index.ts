64cd945bbbaea43176ae5697fb7254d0179dfe0b
Ryker Swensen <83606669+RykerSwensen@users.noreply.github.com>
2023-11-04 11:47:52 -0600

    sets

--- db/index.ts
+++ db/index.ts
@@ -1,15 +1,18 @@
 import dbConfig from '../config/db.config';
 import mongoose from 'mongoose';
 import UserModel from './user';
+import SetModel from './sets'
 
 const db: {
   mongoose: typeof mongoose;
   url: string;
   user: any;
+  sets: any;
 } = {
   mongoose,
   url: dbConfig.url,
-  user: UserModel,   
+  user: UserModel,
+  sets: SetModel   
 };
 
 export default db;
